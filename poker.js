991b4fcba37fc8910c9480e3c93baa590380bceb
louisn973 <louis.n.saunier@gmail.com>
2024-09-11 14:09:11 +0200

    graphic part done with html and css

---
+++ poker.js
@@ -0,0 +1,19 @@
+/*
+
+create a array with all card maybe an array of object
+it will be the card that the user use in the gui
+
+
+Create a function for 
+
+CPT : cout par tour
+    Big Blind + small blind +ante*nb de joueur
+
+ISJ : indice du statue en jeton
+    le nomre de fois que j'ai le cpt dans mon tapis
+    tapis a diviser par cpt et savoir si c'est 4 10 ou 30 
+
+
+calculer la fonction qui fait derouler la partie
+
+*/
