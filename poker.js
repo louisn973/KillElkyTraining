cd6cd2e71072bc2bf00b82880b452800d74951c1
louisn973 <louis.n.saunier@gmail.com>
2024-09-12 15:42:48 +0200

    working on how to have the card value in a var

--- poker.js
+++ poker.js
@@ -1,82 +1,77 @@
-levels = document.getElementById("levels").onclick();
-smallBlind = document.getElementById("small-blind").onclick() ;
-bigBlind = document.getElementById("Big-Blind").onclick();
-ante = document.getElementById("ante").onclick();
-position = document.getElementById("position").onclick();
-tapis = document.getElementById("tapis").onclick();
-limper = document.getElementById("limper").onclick() ; //return a boolean
+var levelsValue;
+var smallBlindValue;
+var bigBlindValue;
+var anteValue;
+var positionValue;
+var tapisValue;
+var limperValue;
+
+levels = document.getElementById("levels");
+levels.addEventListener("click", levelsFunction);
 
 function levelsFunction() {
-    return levels = document.getElementById("levels").value;
+    levelsValue = Number(document.getElementById("levels").value);
+    //run the function that choose the action to do
 }
+
+smallBlind = document.getElementById("small-blind");
+smallBlind.addEventListener("click", smallBlindFunction);
 
 function smallBlindFunction() {
-    return smallBlind = document.getElementById("small-blind").value;
+    smallBlindValue = Number(document.getElementById("small-blind").value);
 }
-  
+
+bigBlind = document.getElementById("Big-Blind").onclick();
+bigBlind.addEventListener("click", bigBlindFunction);
+
 function bigBlindFunction() {
-    return bigBlind = document.getElementById("Big-Blind").value;
+    bigBlindValue =Number(document.getElementById("Big-Blind").value);
 }
+
+ante = document.getElementById("ante").onclick();
+ante.addEventListener("click",anteFunction);
 
 function anteFunction () {
-    return ante = document.getElementById("ante").value;
+    anteValue = Number(document.getElementById("ante").value);
 }
 
-function positionFunction () {
-    return position = document.getElementById("position").value;
+position = document.getElementById("position").onclick();
+position.addEventListener("click", positionFunction);
+
+function positionFunction() {
+    positionValue = Number(document.getElementById("position").value);
 }
+
+tapis = document.getElementById("tapis").onclick();
+tapis.addEventListener("click", tapisFunction)
 
 function tapisFunction() {
-    return tapis = document.getElementById("tapis").value;
+    tapisValue = Number(document.getElementById("tapis").value);
 }
+
+limper = document.getElementById("limper").onclick() ; //return a boolean
+limper.addEventListener("click", limperFunction);
 
 function limperFunction() {
-    //return true if there is limper 
-}
-
-function cardOne(value, color) {
-    return cardOne = [value, color];
-}
-
-function cardTwo(value, color) {
-    return cardTwo = [value, color];
-}
-
-function groupeOneTest (cardOne, cardTwo){
+    if (document.getElementById("limper").value === "y") {
+        limperValue = true;        
+    } else {
+        limperValue = false;
+    }
 }
 
 
+var cardOneValue;
+var cardOneColor;
 
-/*
-
-faire dans le code html deux emplacement pour
-les card avec une liste de valeur et une lise de 
-color qui donneront direcement les cardes
-
-*/
+var cardTwoValue;
+var cardTwoColor;
 
 
+function cardOne(cardOneValue, cardOneColor) {
+    return cardOne = [cardOneValue, cardOneColor];
+}
 
-
-
-
-
-
-/*
-
-create a array with all card maybe an array of object
-it will be the card that the user use in the gui
-
-
-Create a function for 
-
-CPT : cout par tour
-    Big Blind + small blind +ante*nb de joueur
-
-ISJ : indice du statue en jeton
-    le nomre de fois que j'ai le cpt dans mon tapis
-    tapis a diviser par cpt et savoir si c'est 4 10 ou 30 
-
-
-calculer la fonction qui fait derouler la partie
-*/
+function cardTwo(cardTwoValue, cardTwoColor) {
+    return cardTwo = [cardTwoValue, cardTwoColor];
+}
